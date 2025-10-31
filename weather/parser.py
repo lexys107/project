e95ea89952c5887ad8430802875db4624e5fa6e9
lexys107 <lesha.sedykh.0524@mail.ru>
2025-10-31 17:21:06 +0300

    create parser

--- weather/parser.py
+++ weather/parser.py
@@ -1,44 +1,16 @@
 import argparse
 
 def create_parser():
-    """Создать парсер аргументов командной строки"""
-    parser = argparse.ArgumentParser(
-        description='Консольное приложение для показа погоды',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog='''
-Примеры использования:
-  python main.py --city Москва
-  python main.py --coords 55.7558 37.6173
-  python main.py --city Санкт-Петербург --units celsius
-        '''
-    )
-    
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument(
-        '--city',
-        type=str,
-        help='Название города (например: Москва)'
-    )
-    group.add_argument(
-        '--coords',
-        type=float,
-        nargs=2,
-        metavar=('LATITUDE', 'LONGITUDE'),
-        help='Координаты (например: 55.7558 37.6173)'
-    )
-    
-    parser.add_argument(
-        '--units',
-        type=str,
-        choices=['celsius', 'fahrenheit'],
-        default='celsius',
-        help='Единицы измерения температуры (по умолчанию: celsius)'
-    )
-    
-    parser.add_argument(
-        '--no-cache',
-        action='store_true',
-        help='Не использовать кэш'
-    )
-    
+    parser = argparse.ArgumentParser(description="Показ погоды по названию города или координатам.")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # Получение погоды по городу
+    city_parser = subparsers.add_parser("city", help="Показ погоды по названию города")
+    city_parser.add_argument("name", help="Название города")
+
+    # Получение погоды по координатам
+    coords_parser = subparsers.add_parser("coords", help="Показ погоды по координатам")
+    coords_parser.add_argument("lat", type=float, help="Широта")
+    coords_parser.add_argument("lon", type=float, help="Долгота")
+
     return parser
